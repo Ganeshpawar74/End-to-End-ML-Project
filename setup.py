b53a99f3d841088dbf7eeebe481834cfc2c651a2
Ganeshpawar74 <gp1237114@gmail.com>
2025-08-12 18:43:36 +0530

    setup

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     name="mlproject",
     version="0.0.1",
     author="Ganesh",
-    author_email="ganeshppawar864@example.com",
+    author_email="ganeshppawar864@gmail.com",
     packages=find_packages(),
     install_requires=get_requirements("requirements.txt")
 )
